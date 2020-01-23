350694d7ab5df60508c75af97091b335189989e1
Student <example@example.com>
2020-01-23 14:10:02 -0800

    sliced the first character in an array, capitalized it, and added ay

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,32 @@ var toLowerCase = function(sentence) {
   return sentence.toLowerCase();
 };
 
+var wordsToLetters = function(word) {
+  letter = word.split("");
 
+  for ( var index = 0; index === 0 ; index +=1   ) {
+
+    if (letter[0] != "a" || letter[0] != "e" || letter[0] != "i" || letter[0] != "o" || letter[0] != "u") {
+     var firstChar = $(letter.slice(0,1));
+     var pigLatinEnding = (firstChar[0].toUpperCase() + "ay");
+      console.log(pigLatinEnding);
+    } else {
+
+    }
+    
+    // (letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter === "u")
+  }
+            
+};
 
 $(document).ready(function () {
   $("form#pigLatinForm").submit(function () {
-     var input = $("input#sentence").val();
-     input = toLowerCase(input);
-     console.log(input);
-     event.preventDefault();
+    var input = $("input#sentence").val();
+    input = toLowerCase(input);
+    letters = wordsToLetters(input);
+
+
+    
+    event.preventDefault();
   });
-});
+}); 
